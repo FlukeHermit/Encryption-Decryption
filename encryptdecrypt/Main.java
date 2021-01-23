278df8a7ffc001294d9d787b72b33d28e9ce27d3
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-01-24 00:27:41 +0600

    Command line

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -2,25 +2,45 @@ package encryptdecrypt;
 
 import java.util.Scanner;
 
-public class Main {    
+public class Main {
     static char begin = 32;
     static char end = '~';
     static int size = 95;
+    static String option = "enc";
+    static String shift = "0";
+    static String text = "";
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String option = sc.nextLine();
-        String text = sc.nextLine();
-        char[] chars = text.toCharArray();
-        int shift = sc.nextInt();
 
-        if (option.equals("enc")) {
-            encrypt(chars, shift);
-        } else if (option.equals("dec")) {
-            decrypt(chars, shift);        
+        for (int i = 0; i < args.length; i += 2) {
+            if (!args[i].startsWith("-") || args[i + 1].startsWith("-")) {
+                System.out.println("Error: Illegal Arguments");
+                break;
+            }
+
+            switch (args[i]) {
+                case "-mode":
+                    option = args[i+1];
+                    break;
+                case "-key":
+                    shift = args[i+1];
+                    break;
+                case "-data":
+                    text = args[i+1];
+                    break;
+            }
+
         }
 
+        char[] chars = text.toCharArray();
+
+        if (option.equals("enc")) {
+            encrypt(chars, Integer.valueOf(shift));
+        } else if (option.equals("dec")) {
+            decrypt(chars, Integer.valueOf(shift));
+        }
 
     }
 
